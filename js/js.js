eb4115bf34eb582604fbd2b0bbeb0d25e53d533e
satyajit <satyajit_jbl@yahoo.com>
2024-09-23 21:53:11 +0600

    all card done

--- js/js.js
+++ js/js.js
@@ -1,1 +1,122 @@
-console.log('js file added');
+//function to get input value
+function getInputValueById(id){
+    return parseFloat(document.getElementById(id).value);
+};
+
+//function to get text value
+function getTextValueByID(id){
+    return parseFloat(document.getElementById(id).innerText)
+};
+
+// faq
+
+
+//function for error handaling
+
+
+// const donationNoakhali = getInputValueById('donation-noakhali');
+// const totalDonationNoakhali = getTextValueByID('total-donation-noakhali');
+// // console.log(totalDonationNoakhali, donationNoakhali);
+
+
+const calculateNoakhali = document.getElementById('btn-donate-noakhali');
+calculateNoakhali.addEventListener('click', function(){
+    const donationNoakhali = getInputValueById('donation-noakhali');
+    const totalDonationNoakhali = getTextValueByID('total-donation-noakhali');
+    const amount = getTextValueByID('amount');
+
+    if(donationNoakhali<=0 || isNaN(donationNoakhali) || donationNoakhali > amount || donationNoakhali == ""){
+        alert('Invalid Donation amount');
+        return;
+    }
+
+    // console.log(totalDonationNoakhali, donationNoakhali);
+
+    GtotalDonationNoakhali = donationNoakhali + totalDonationNoakhali;
+    FinalAmount = amount - donationNoakhali;
+    console.log(GtotalDonationNoakhali);
+
+    const totalDonationNoakhaliEl = document.getElementById('total-donation-noakhali');
+    totalDonationNoakhaliEl.innerText = GtotalDonationNoakhali;
+
+    const amountEl = document.getElementById('amount');
+    amountEl.innerText = FinalAmount;
+
+    // History part 1 start
+
+    // const p = document.createElement('p');
+    // p.innerText = `${donationNoakhali} Taka is Donated for Flood relief in Noakhali, Bangladesh`;
+    // console.log(p);
+    // document.getElementById('history-section').appendChild(p);
+
+    //date include
+    // const d = new Date();
+    // const h1 = document.createElement('h1');
+    
+    // h1.innerText = d;
+    // document.getElementById('history-section').appendChild(h1);
+       // History part 1 end
+
+          // History part 2 start
+
+          const historyItem = document.createElement('div');
+          historyItem.className = " bg-white p-3 rounded-md border-2 border-gray-300"
+          historyItem.innerHTML = `
+          <p text-xl font-bold text-black>${donationNoakhali} Taka is Donated for Flood relief in Noakhali, Bangladesh </p>
+          
+          <p text-xs text-gray-500>${new Date()} </p>
+          `
+          const historyContainer = document.getElementById('history-section');
+          historyContainer.insertBefore(historyItem, historyContainer.firstChild);
+
+
+    // Modal show
+    // let modalEl = document.getElementById('modal');
+    document.getElementById('modal').classList.remove('hidden');
+
+    
+    // totalDonationNoakhaliEl.innerText = totalDonationNoakhali;
+    // console.log('noakhali button clicked');
+
+})
+
+
+// history tab
+
+const historyTab = document.getElementById('history-tab');
+const donationTab = document.getElementById('donation-tab');
+
+historyTab.addEventListener('click', function(){
+    historyTab.classList.add(
+        // 'text-black',
+        'bg-green-400'
+    );
+
+    donationTab.classList.remove(
+        'bg-green-400'
+    );
+
+    document.getElementById('donation-section').classList.add('hidden');
+    document.getElementById('history-section').classList.remove('hidden');
+})
+
+donationTab.addEventListener('click', function(){
+    historyTab.classList.remove(
+        'bg-green-400'
+    );
+
+    donationTab.classList.add(
+        'bg-green-400'
+    );
+
+    document.getElementById('donation-section').classList.remove('hidden');
+    document.getElementById('history-section').classList.add('hidden');
+})
+
+// function to close modal- popup
+const btnCloseModal = document.getElementById('btn-close-modal')
+btnCloseModal.addEventListener('click', function(){
+    document.getElementById('modal').classList.add('hidden');
+    // console.log('button modal clicked');
+})
+// document.getElementById('btn-close-modal').classList.add('hidden');
